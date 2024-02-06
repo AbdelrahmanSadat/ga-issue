29ddfe1f3e19b3f24a0c64ad931ee02f5a310419
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2024-02-06 08:56:08 +0200

    - minimum reproduction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
       <body>{children}</body>
+      <GoogleAnalytics gaId={"G-XXXXXXXX"}/>
+
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 /** Add your relevant code here for the issue to reproduce */
+"use client";
+import { sendGAEvent } from "@next/third-parties/google"
+
 export default function Home() {
-  return null;
+  return <>
+    <button onClick={() => sendGAEvent({event: "page_view", value: ''})}>Send event with sendGAEvent</button>
+    <button onClick={() => (window as any).gtag("event", "page_view", {value: ''})}>Send event with window.gtag</button>
+  </>
 }
